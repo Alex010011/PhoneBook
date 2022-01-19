23b00b35434f9c2a71cbfcac6d741c0121bb5931
Alex010011 <bunkof3@yandex.ru>
2022-01-20 01:28:08 +0200

    HW190122

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -14,7 +14,7 @@ public class ApplicationManager {
         wd = new ChromeDriver();
         wd.manage().window().maximize();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+       // wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         userHelper = new UserHelper(wd);
     }
 
